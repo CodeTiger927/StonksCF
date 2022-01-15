cb58d460d33c0f46c2db67243ee5494cfcd64a64
L0ad1n6 <altan.mehmet.unver@gmail.com>
2022-01-15 12:55:05 -0500

    [Fix] Added helper functions that I forgot to move

--- DiscordBot/commands/verify.js
+++ DiscordBot/commands/verify.js
@@ -4,6 +4,27 @@ const md5 = require("md5");
 const users = require("./users.json");
 const APIURL = "https://codetiger.me/project/StonksCF/backend/API.php";
 
+function saveUsers() {
+	fs.writeFile("users.json",JSON.stringify(users),(err) => {
+		if(err) console.log(err);
+		console.log("Saving new user");
+	});
+}
+
+function numberWithCommas(x) {
+	var parts = x.toString().split(".");
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return parts.join(".");
+}
+
+function reformNum(number) {
+	return numberWithCommas(Math.round(number * 100) / 100);
+}
+
+function nameToCode(name) {
+	return "[" + name + "](https://codetiger.me/project/StonksCF/explore.html?username=" + name + ")"; 
+}
+
 module.exports = {
     name: 'verify',
     description: 'Link your StonksCF account',
